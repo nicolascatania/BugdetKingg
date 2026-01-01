11473c5f3b02942006d90bb675f100dddd6db0ac
nicolascatania <nicolas20032401@gmail.com>
2026-01-01 14:26:58 -0300

    add new backend

---
+++ BudgetKing/src/main/java/com/veritech/BudgetKing/BudgetKingApplication.java
@@ -0,0 +1,13 @@
+package com.veritech.BudgetKing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BudgetKingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BudgetKingApplication.class, args);
+	}
+
+}
---
+++ BudgetKing/src/test/java/com/veritech/BudgetKing/BudgetKingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.veritech.BudgetKing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BudgetKingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
