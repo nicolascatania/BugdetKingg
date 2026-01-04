a3c2817f89868b3b6bd1baa8301fcb69e0f3533d
nicolascatania <nicolas20032401@gmail.com>
2026-01-04 16:30:44 -0300

    dashboard viewv

--- frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,10 +1,53 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
 
+Chart.register(PieController, ArcElement, Tooltip, Legend);
 @Component({
-  selector: 'dashboard',
+  selector: 'app-dashboard',
   imports: [],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class Dashboard { }
+export class Dashboard {
+
+
+  fromDate!: string;
+  toDate!: string;
+
+  ngOnInit() {
+    const now = new Date();
+
+    const firstDay = new Date(now.getFullYear(), now.getMonth(), 1);
+    const lastDay = new Date(now.getFullYear(), now.getMonth() + 1, 0);
+
+    this.fromDate = firstDay.toISOString().split('T')[0];
+    this.toDate = lastDay.toISOString().split('T')[0];
+  }
+
+  ngAfterViewInit() {
+    new Chart('expensePie', {
+      type: 'pie',
+      data: {
+        labels: ['Purchase', 'Investment', 'Transfer', 'Adjustment'],
+        datasets: [{
+          data: [45000, 30000, 15000, 10000],
+        }]
+      },
+      options: {
+        responsive: true,
+        maintainAspectRatio: false,
+        plugins: {
+          legend: {
+            position: 'bottom',
+            labels: {
+              boxWidth: 12,
+              padding: 12
+            }
+          }
+        }
+      }
+    });
+
+  }
+}
