562a221a9629a08b3a7b2d504eb36119ea3d7481
nicolascatania <nicolas20032401@gmail.com>
2026-01-04 17:15:05 -0300

    input of my own

--- frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
+++ frontend/src/app/features/dashboard/pages/dashboard/dashboard.ts
@@ -1,18 +1,22 @@
-import { AfterViewInit, ChangeDetectionStrategy, Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { AfterViewInit, ChangeDetectionStrategy, Component, HostListener } from '@angular/core';
 import { Chart, PieController, ArcElement, Tooltip, Legend } from 'chart.js';
 
-declare var TomSelect: any;
 
 
 Chart.register(PieController, ArcElement, Tooltip, Legend);
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Dashboard implements AfterViewInit {
+  categories = ['Food', 'Transport', 'Services', 'Investment'];
+  selectedCategories: string[] = [];
+  dropdownOpen = false;
+
 
 
   fromDate!: string;
@@ -29,13 +33,6 @@ export class Dashboard implements AfterViewInit {
   }
 
   ngAfterViewInit() {
-
-    new TomSelect('#category-select', {
-      plugins: ['remove_button'], // Botón para quitar cada tag
-      persist: false,
-      create: false,
-      placeholder: 'Select categories...',
-    });
 
     new Chart('expensePie', {
       type: 'pie',
@@ -60,5 +57,31 @@ export class Dashboard implements AfterViewInit {
       }
     });
 
+  }
+
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  selectCategory(cat: string) {
+    if (!this.selectedCategories.includes(cat)) {
+      this.selectedCategories.push(cat);
+    } else {
+      this.removeCategory(cat);
+    }
+  }
+
+  removeCategory(cat: string) {
+    this.selectedCategories = this.selectedCategories.filter(c => c !== cat);
+  }
+
+  @HostListener('document:click', ['$event'])
+  handleClickOutside(event: MouseEvent) {
+    const target = event.target as HTMLElement;
+    // Si el click no fue dentro de tu dropdown, cerralo
+    if (!target.closest('.category-dropdown')) {
+      this.dropdownOpen = false;
+    }
   }
 }
