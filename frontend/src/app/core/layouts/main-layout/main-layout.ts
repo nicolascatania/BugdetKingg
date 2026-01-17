f40c531db5afbb45b425864972f7ef02483df771
nicolascatania <nicolas20032401@gmail.com>
2026-01-17 14:37:35 -0300

    toggle sidebar

--- frontend/src/app/core/layouts/main-layout/main-layout.ts
+++ frontend/src/app/core/layouts/main-layout/main-layout.ts
@@ -1,4 +1,4 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, signal } from '@angular/core';
 import { SideBar } from '../../../shared/components/side-bar/side-bar';
 import { RouterOutlet } from '@angular/router';
 
@@ -9,4 +9,10 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './main-layout.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class MainLayout { }
+export class MainLayout {
+  sidebarOpen = signal(false);
+
+  toggleSidebar() {
+    this.sidebarOpen.update(v => !v);
+  }
+}
--- frontend/src/app/shared/components/side-bar/side-bar.ts
+++ frontend/src/app/shared/components/side-bar/side-bar.ts
@@ -1,24 +1,28 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, output } from '@angular/core';
 import { Router, RouterLink, RouterLinkActive } from '@angular/router';
 import { AuthService } from '../../../core/services/auth';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'side-bar',
-  imports: [RouterLinkActive, RouterLink],
+  imports: [RouterLinkActive, RouterLink, CommonModule],
   templateUrl: './side-bar.html',
   styleUrl: './side-bar.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class SideBar {
+  sidebarOpen = input.required<boolean>();
+  toggleSideBarEvent = output<void>();
 
   constructor(
     private authService: AuthService,
     private router: Router
   ) { }
 
-  /**
-   * Logs out the current user and redirects to login page.
-   */
+  toggleSidebar() {
+    this.toggleSideBarEvent.emit();
+  }
+
   logout(): void {
     this.authService.logout();
     this.router.navigate(['/login']);
