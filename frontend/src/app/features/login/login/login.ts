416eb97a7fbc98f653e45a3e3060bd2eb53cc349
nicolascatania <nicolas20032401@gmail.com>
2026-06-14 22:01:10 -0300

    login and register page

--- frontend/src/app/features/login/login/login.ts
+++ frontend/src/app/features/login/login/login.ts
@@ -1,43 +1,38 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '../../../core/services/auth';
-import { FormBuilder, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { Router, RouterLink } from '@angular/router';
-import { LoginRequest } from '../interfaces/login.interface';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-login',
-  imports: [FormsModule, CommonModule, ReactiveFormsModule, RouterLink],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './login.html',
   styleUrl: './login.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Login {
-
   loginForm: FormGroup;
-
-  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+  ) {
     this.loginForm = this.fb.group({
       email: ['', [Validators.email, Validators.required]],
-      password: ['', [Validators.required]]
+      password: ['', [Validators.required]],
     });
   }
-
   onSubmit(): void {
     if (this.loginForm.invalid) return;
-
-    const request: LoginRequest = this.loginForm.value;
-
-
-    this.authService.login(request).subscribe({
-      next: () => {
-        console.log('Login succesful');
-        this.router.navigate(['/home']);
-      },
-      error: (err) => {
-        console.error('Login error', err);
-      }
+    this.authService.login(this.loginForm.value).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (err) => console.error('Login error', err),
     });
   }
-
 }
--- frontend/src/app/features/login/register/register.ts
+++ frontend/src/app/features/login/register/register.ts
@@ -1,44 +1,40 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { AuthService } from '../../../core/services/auth';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
-import { Router } from '@angular/router';
-import { RegisterRequest } from '../interfaces/login.interface';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { Router, RouterLink } from '@angular/router';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-register',
-  imports: [ReactiveFormsModule],
+  imports: [CommonModule, ReactiveFormsModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class Register {
-
   registerForm: FormGroup;
-
-  constructor(private authService: AuthService, private fb: FormBuilder, private router: Router) {
+  constructor(
+    private authService: AuthService,
+    private fb: FormBuilder,
+    private router: Router,
+  ) {
     this.registerForm = this.fb.group({
       email: ['', [Validators.email, Validators.required]],
       password: ['', [Validators.required]],
       name: ['', [Validators.required]],
-      lastName: ['', [Validators.required]]
+      lastName: ['', [Validators.required]],
     });
   }
-
   onSubmit(): void {
     if (this.registerForm.invalid) return;
-
-    const request: RegisterRequest = this.registerForm.value;
-
-
-    this.authService.register(request).subscribe({
-      next: () => {
-        console.log('Succesful register');
-        this.router.navigate(['/home']);
-      },
-      error: (err) => {
-        console.error('Register error', err);
-      }
+    this.authService.register(this.registerForm.value).subscribe({
+      next: () => this.router.navigate(['/home']),
+      error: (err) => console.error('Register error', err),
     });
   }
-
 }
