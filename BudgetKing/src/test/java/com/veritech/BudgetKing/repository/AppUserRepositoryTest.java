18f771da9c8bf686347646c25284049368d5cddb
nicolascatania <nicolas20032401@gmail.com>
2026-07-18 18:19:46 -0300

    docs

--- BudgetKing/src/test/java/com/veritech/BudgetKing/repository/AppUserRepositoryTest.java
+++ BudgetKing/src/test/java/com/veritech/BudgetKing/repository/AppUserRepositoryTest.java
@@ -1,8 +1,12 @@
 package com.veritech.BudgetKing.repository;
 
+import com.veritech.BudgetKing.model.AppUser;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.data.jpa.test.autoconfigure.DataJpaTest;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -14,6 +18,7 @@ class AppUserRepositoryTest extends BaseRepositoryTest {
     private AppUserRepository repository;
 
     @Test
+    @DisplayName("Find user by email")
     void findByEmail() {
         var result = repository.findByEmail("nico@budgetking.com");
         assertTrue(result.isPresent(), "User should be found");
@@ -24,8 +29,10 @@ class AppUserRepositoryTest extends BaseRepositoryTest {
     }
 
     @Test
+    @DisplayName("Determine if an user exists using email")
     void existsByEmail() {
         boolean result = repository.existsByEmail("nico@budgetking.com");
         assertTrue(result, "User should exist");
     }
+
 }
